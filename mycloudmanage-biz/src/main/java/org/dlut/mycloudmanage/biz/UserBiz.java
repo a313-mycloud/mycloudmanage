b8ab25d51183b0d196629da37ef7b349a885bd45
luojie <luojie715@foxmail.com>
2014-11-21 16:19:40 +0800

    新增UserBiz

---
+++ mycloudmanage-biz/src/main/java/org/dlut/mycloudmanage/biz/UserBiz.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2014 etao.com All right reserved. This software is the
+ * confidential and proprietary information of etao.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with etao.com .
+ */
+package org.dlut.mycloudmanage.biz;
+
+/**
+ * 类UserBiz.java的实现描述：TODO 类实现描述
+ * 
+ * @author luojie 2014年11月21日 下午4:18:29
+ */
+public class UserBiz {
+
+}

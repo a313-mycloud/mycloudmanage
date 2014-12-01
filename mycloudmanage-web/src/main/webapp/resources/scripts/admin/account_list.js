26be8d92d3f39db2ee206573db8fb40250fc9e90
luojie <luojie715@foxmail.com>
2014-12-01 14:38:35 +0800

    js

--- mycloudmanage-web/src/main/webapp/resources/scripts/admin/account_list.js
+++ mycloudmanage-web/src/main/webapp/resources/scripts/admin/account_list.js
@@ -1,13 +1,11 @@
 $(document).ready(function(){
 	$(".delete_btn").click(function () {
-		confirm("确定删除？");
-		var id = $(this).attr("id").split("_")[1];
-		alert(id);
+		var account = $(this).attr("user_id");
+		var r=false; if(confirm('确定删除' + account + '吗？?')) r=true; event.returnValue=r; return r;
 	});
 });
 
 function deleteAction() {
    var r=false; if(confirm('确定删除吗？?')) r=true; event.returnValue=r; return r;
-        //confirm("确定删除吗？");
 	
 }
